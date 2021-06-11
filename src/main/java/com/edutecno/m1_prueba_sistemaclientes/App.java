c146a7849dffed8dc77d5e73dd44e29406965dcc
Christian Rodríguez Bugueño <c_rodriguez_b@yahoo.com>
2021-06-11 16:25:09 -0400

    primer commit desde eclipse

---
+++ src/main/java/com/edutecno/m1_prueba_sistemaclientes/App.java
@@ -0,0 +1,13 @@
+package com.edutecno.m1_prueba_sistemaclientes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
